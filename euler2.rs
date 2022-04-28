e30fe43a95a3061972d5b698082499b2440988a0
Izaak Walton <izaakw@protonmail.com>
2022-04-28 13:22:46 -0600

    first commit, figuring out cargo

---
+++ euler2.rs
@@ -0,0 +1,24 @@
+//Euler # 2
+
+fn euler2() -> u32 {
+    /* ?Find the sum of all even fibonacci numbers under 4 million*/
+    let mut fib = 1;
+    let mut last_fib = 1;
+    let mut temp;
+
+    let mut fibsum = 0;
+    
+    while fib < 100 {
+	if fib % 2 == 0 {
+	    fibsum += fib;
+	    temp = fib;
+	    fib += last_fib;
+	    last_fib = temp;
+	}
+    }
+    return fibsum;
+}
+
+fn main() {
+    println!("Euler # 2: {}", euler2().to_string());
+}
---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+// lib.rs
+#![crate_name = "project_euler_rust"]
+mod euler1;
+
+pub use crate::euler1::euler1;
+
+mod euler2;
+
+pub use crate::euler2::euler2;
